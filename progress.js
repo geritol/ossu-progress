c329980cb6f55f28a028a09b86528cdfc9eaf33b
geritol <gergo.tolnai@gmail.com>
2018-02-04 17:08:27 +0100

    init

---
+++ progress.js
@@ -0,0 +1,168 @@
+'use strict';
+
+var progress = [
+	{
+		name: 'Introduction to Computer Science',
+		courses: [
+		{
+			name: 'Introduction to Computer Science - CS50',
+			url: 'https://www.edx.org/course/introduction-computer-science-harvardx-cs50x#!',
+			status: 'done'
+		},
+		{
+			name: 'Introduction to Computer Science and Programming using Python',
+			url: 'https://www.edx.org/course/introduction-computer-science-mitx-6-00-1x-10',
+			status: 'done'
+		}
+	]},
+	{
+		name: 'Core CS',
+		categories: [
+			{
+				name: 'Core programming',
+				courses: [
+					{
+						name: 'How to Code - Simple Data',
+						url: 'https://www.edx.org/course/how-code-simple-data-ubcx-htc1x',
+						status: 'doing'
+					},
+					{
+						name: 'How to Code - Complex Data',
+						url: 'https://www.edx.org/course/how-code-complex-data-ubcx-htc2x',
+						status: 'todo'
+					},
+					{
+						name: 'Software Construction - Data Abstraction',
+						url: 'https://www.edx.org/course/software-construction-data-abstraction-ubcx-softconst1x',
+						status: 'todo'
+					},
+					{
+						name: 'Software Construction - Object-Oriented Design',
+						url: 'https://www.edx.org/course/software-construction-object-oriented-ubcx-softconst2x',
+						status: 'todo'
+					},
+					{
+						name: 'Programming Languages, Part A',
+						url: 'https://www.coursera.org/learn/programming-languages',
+						status: 'done'
+					},
+					{
+						name: 'Programming Languages, Part B',
+						url: 'https://www.coursera.org/learn/programming-languages-part-b',
+						status: 'done'
+					},
+					{
+						name: 'Programming Languages, Part C',
+						url: 'https://www.coursera.org/learn/programming-languages-part-c',
+						status: 'done'
+					},
+				]
+			},
+			{
+				name: 'Core math',
+				courses: [
+					{
+						name: 'Essence of Linear Algebra',
+						url: 'https://www.youtube.com/playlist?list=PLZHQObOWTQDPD3MizzM2xVFitgF8hE_ab',
+						status: 'todo'
+					},
+					{
+						name: 'Linear Algebra - Foundations to Frontiers',
+						url: 'https://www.edx.org/course/linear-algebra-foundations-frontiers-utaustinx-ut-5-04x#!',
+						status: 'todo'
+					},
+					{
+						name: 'Calculus One',
+						url: 'https://www.coursera.org/learn/calculus1',
+						status: 'done'
+					},
+					{
+						name: 'Calculus Two: Sequences and Series',
+						url: 'https://www.coursera.org/learn/advanced-calculus',
+						status: 'todo'
+					},
+					{
+						name: 'Mathematics for Computer Science',
+						url: 'https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-042j-mathematics-for-computer-science-spring-2015/index.htm',
+						status: 'todo'
+					}
+				]
+			},
+			{
+				name: 'Core systems',
+				courses: [
+					{
+						name: 'Build a Modern Computer from First Principles: From Nand to Tetris',
+						url: 'https://www.coursera.org/learn/build-a-computer',
+						status: 'todo'
+					},
+					{
+						name: 'Build a Modern Computer from First Principles: Nand to Tetris Part II',
+						url: 'https://www.coursera.org/learn/nand2tetris2',
+						status: 'todo'
+					},
+					{
+						name: 'Introduction to Computer Networking',
+						url: 'https://lagunita.stanford.edu/courses/Engineering/Networking-SP/SelfPaced/about',
+						status: 'todo'
+					},
+					{
+						name: 'ops-class.org - Hack the Kernel',
+						url: 'https://www.ops-class.org/',
+						status: 'todo'
+					}
+				]
+			},
+			{
+				name: 'Core theory',
+				courses: [
+					{
+						name: 'Algorithms: Design and Analysis, Part I',
+						url: 'https://lagunita.stanford.edu/courses/course-v1:Engineering+Algorithms1+SelfPaced/about',
+						status: 'done'
+					},
+					{
+						name: 'Algorithms: Design and Analysis, Part II',
+						url: 'https://lagunita.stanford.edu/courses/course-v1:Engineering+Algorithms2+SelfPaced/about',
+						status: 'todo'
+					}
+				]
+			},
+			{
+				name: 'Core applications',
+				courses: [
+					{
+						name: 'Databases',
+						url: 'https://lagunita.stanford.edu/courses/DB/2014/SelfPaced/about',
+						status: 'done'
+					},
+					{
+						name: 'Machine Learning',
+						url: 'https://www.coursera.org/learn/machine-learning',
+						status: 'todo'
+					},
+					{
+						name: 'Computer Graphics',
+						url: 'https://www.edx.org/course/computer-graphics-uc-san-diegox-cse167x',
+						status: 'todo'
+					},
+					{
+						name: 'Cryptography I',
+						url: 'https://www.coursera.org/course/crypto',
+						status: 'todo'
+					},
+					{
+						name: 'Software Engineering: Introduction',
+						url: 'https://www.edx.org/course/software-engineering-introduction-ubcx-softeng1x',
+						status: 'todo'
+					},
+					{
+						name: 'Software Development Capstone Project',
+						url: 'https://www.edx.org/course/software-development-capstone-project-ubcx-softengprjx',
+						status: 'todo'
+					}
+				]
+			}
+		]
+	}
+]
